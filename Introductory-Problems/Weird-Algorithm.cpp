a02c44ed954ade643123cde278bbd31a090a425e
RedwanPlague <mdredwan.rh@gmail.com>
2021-01-26 20:36:37 +0600

    added .gitignore. Solved problem 'Weird Algorithm'

---
+++ Introductory-Problems/Weird-Algorithm.cpp
@@ -0,0 +1,22 @@
+// https://cses.fi/problemset/task/1083
+
+#include <iostream>
+
+using namespace std;
+
+typedef long long ll;
+
+int main () {
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+    ll n;
+    cin >> n;
+    while (n > 1) {
+        cout << n << " ";
+        n = (n & 1) ? 3 * n + 1 : n / 2;
+    }
+    cout << 1 << endl;
+
+    return 0;
+}
+
