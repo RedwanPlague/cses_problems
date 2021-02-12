a8047040188f0a9e8f0898d4681a04b31634aa02
RedwanPlague <mdredwan.rh@gmail.com>
2021-02-12 14:23:22 +0600

    solved 'Hamiltonian Flights'. dp[MX][1<<MX] got TLE, but dp[1<<MX][MX] passed. I'm not sure if keeping the larger dimension infront always helps with cache miss troubles.

---
+++ Graph-Algorithms/Hamiltonian-Flights.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+#define MX 20
+#define MOD 1000000007
+
+int n;
+int g[MX][MX];
+
+// first state = mask of which nodes i have visited so far, including current
+// second state = which node i am currently at
+int dp[1 << MX][MX];
+
+int ways (const int mask, const int u) {
+    if (dp[mask][u] != -1) return dp[mask][u];
+
+    int ret = 0;
+    for (int i=0; i<n; i++) {
+        if ((mask & (1 << i)) && g[i][u]) {
+            // go back the i -> u edge.
+            // last node is now i, u is now unvisited
+            ret = (ret + (long long)g[i][u] * ways(mask ^ (1 << u), i) % MOD) % MOD;
+        }
+    }
+    return (dp[mask][u] = ret);
+}
+
+int main () {
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif
+
+    int m;
+    cin >> n >> m;
+
+    for (int i=0; i<m; i++) {
+        int u, v;
+        cin >> u >> v;
+        u--; v--;
+        if (u != v) { // no point of keeping self loops
+            g[u][v]++;
+        }
+    }
+
+    memset(dp, -1, sizeof(dp));
+
+    // we start at 0
+    // 1 way of ending up in 0, with only 0 visited
+    dp[1][0] = 1;
+    for (int i=1; i<n; i++) {
+        // no way of ending up in i, with only i visited
+        // because 0 should have been visited too
+        dp[1 << i][i] = 0;
+    }
+
+    cout << ways((1 << n) - 1, n-1) << endl;
+
+    return 0;
+}
+
