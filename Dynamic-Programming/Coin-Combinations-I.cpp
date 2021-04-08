3b6470139df8ce5b2a20b0f166915d281c2158cc
RedwanPlague <mdredwan.rh@gmail.com>
2021-04-08 17:46:57 +0600

    formatted with coc-clangd

--- Dynamic-Programming/Coin-Combinations-I.cpp
+++ Dynamic-Programming/Coin-Combinations-I.cpp
@@ -4,29 +4,30 @@
 
 using namespace std;
 
-#define N 100
-#define X 1000001
+#define N   100
+#define X   1000001
 #define MOD 1000000007
 
 int coin[N];
 int dp[X];
 
-int main () {
-    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
 
     int n, x;
     cin >> n >> x;
 
-    for (int i=0; i<n; i++) {
+    for (int i = 0; i < n; i++) {
         cin >> coin[i];
     }
 
     dp[0] = 1;
-    for (int i=1; i<=x; i++) {
+    for (int i = 1; i <= x; i++) {
         dp[i] = 0;
-        for (int j=0; j<n; j++) {
+        for (int j = 0; j < n; j++) {
             if (i >= coin[j]) {
-                dp[i] += dp[i-coin[j]];
+                dp[i] += dp[i - coin[j]];
                 if (dp[i] >= MOD) {
                     dp[i] -= MOD;
                 }
@@ -38,4 +39,3 @@ int main () {
 
     return 0;
 }
-
