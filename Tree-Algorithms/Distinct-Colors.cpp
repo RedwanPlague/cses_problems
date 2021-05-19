002b9e3b7cf024349214bdef51a44b37b02fad5a
RedwanPlague <mdredwan.rh@gmail.com>
2021-05-19 09:55:57 +0600

    solved a classic dsu-on-tree

--- Tree-Algorithms/Distinct-Colors.cpp
+++ Tree-Algorithms/Distinct-Colors.cpp
@@ -1,7 +1,7 @@
 // https://cses.fi/problemset/task/1139
 
 #include <iostream>
-#include <set>
+#include <map>
 #include <vector>
 
 #include <sys/resource.h>
@@ -13,7 +13,7 @@ using namespace std;
 #define all(v) (v).begin(), (v).end()
 
 vector<vector<int>> adj;
-int sz[MX], col[MX], cnt[MX];
+int sz[MX], col[MX], ans[MX], cnt[MX], total;
 bool big[MX];
 
 void getsz(int u, int p) {
@@ -27,7 +27,9 @@ void getsz(int u, int p) {
 }
 
 void add(int u, int p, int x) {
+    total += x * (cnt[col[u]] == 0);
     cnt[col[u]] += x;
+    total += x * (cnt[col[u]] == 0);
     for (auto v : adj[u]) {
         if (v != p && !big[v]) {
             add(v, u, x);
@@ -53,6 +55,7 @@ void dfs(int u, int p, bool keep) {
         big[bigv] = true;
     }
     add(u, p, 1);
+    ans[u] = total;
     if (bigv != -1) {
         big[bigv] = false;
     }
@@ -68,21 +71,31 @@ int main() {
     freopen("test_input.txt", "r", stdin);
 #endif
 
-    struct rlimit rl;
-    getrlimit(RLIMIT_STACK, &rl);
-    cout << rl.rlim_cur << endl;
-    rl.rlim_cur *= 4;
-    setrlimit(RLIMIT_STACK, &rl);
-    getrlimit(RLIMIT_STACK, &rl);
-    cout << rl.rlim_cur << endl;
+    /* struct rlimit rl; */
+    /* getrlimit(RLIMIT_STACK, &rl); */
+    /* cout << rl.rlim_cur << endl; */
+    /* rl.rlim_cur *= 4; */
+    /* setrlimit(RLIMIT_STACK, &rl); */
+    /* getrlimit(RLIMIT_STACK, &rl); */
+    /* cout << rl.rlim_cur << endl; */
 
     int n;
     cin >> n;
 
     adj.resize(n);
 
+    map<int, int> mp;
     for (int i = 0; i < n; i++) {
         cin >> col[i];
+        auto it = mp.find(col[i]);
+        if (it != mp.end()) {
+            col[i] = it->second;
+        }
+        else {
+            int use = mp.size();
+            mp[col[i]] = use;
+            col[i] = use;
+        }
     }
 
     for (int i = 1; i < n; i++) {
@@ -93,11 +106,14 @@ int main() {
         adj[v].push_back(u);
     }
 
-    dfs(0, -1);
+    getsz(0, -1);
 
-    // for (auto x : ans) {
-    //     cout << x << ' ';
-    // }
+    total = 0;
+    dfs(0, -1, true);
+
+    for (int i = 0; i < n; i++) {
+        cout << ans[i] << ' ';
+    }
 
     return 0;
 }
